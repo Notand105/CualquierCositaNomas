6a8ef6e73c7212670e5c60cab94bf0634b7b93a3
Fernando Urzúa <urzua.fernando.f@gmail.com>
2021-05-06 15:29:05 -0400

    problema521 algoritmos
    apenas un poco de la entrada

---
+++ src/Code/Problema521.java
@@ -0,0 +1,37 @@
+
+package Code;
+
+import java.util.Scanner;
+
+
+public class Problema521 {
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("inicio");
+        Scanner scanner1= new Scanner(System.in);
+        Scanner scanner2= new Scanner(System.in);
+        String cadena1;
+        String cadena2;
+        int n1=0,n2=0,n3=0;
+        
+        do{
+            
+        cadena1=scanner1.nextLine();
+        cadena2=scanner2.nextLine();    
+        System.out.println(cadena1);
+        
+        
+        }while(n1!=0&& n2!=0&&n3!=0);
+        
+        
+        
+        
+    }
+  
+    
+}
