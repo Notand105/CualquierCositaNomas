cb15f02eda0f9b9342aa085c1d7263a5f3390259
Fernando Urzúa <urzua.fernando.f@gmail.com>
2022-06-20 20:48:12 -0400

    quicksort mas ayuda por favor

--- algoritmos/tarea3/quicksort2.java
+++ algoritmos/tarea3/quicksort2.java
@@ -1,14 +1,12 @@
-package tarea3;
 import java.util.Random;
-public class quicksort 
-{
-  public static void main(String[] args) 
+public class quicksort2 {
+     public static void main(String[] args) 
   {
 	long inicio=0;
 	long fin=0;
 	double tiempo=0;
         //las 3 variables de arriba cuentan el tiempo de ejecucion de los codigos
-	int top=90;
+	int top=10000000;
         int pruebas=2;
     int[] array = new int[top]; //{ 12, 13, 24, 10, 3, 6, 90, 70,12,142,32,45,2,12,32,12,12,2323 };
         // arrelgo desordenado
@@ -42,7 +40,7 @@ public class quicksort
         */
     System.out.print(tiempo +" milisegundos");
 	
-        printa(array);System.out.println();
+        //printa(array);System.out.println();
 	shuffle(array, top);
 
 	System.out.print("QuickSort Mediana 2k+1 : ");
@@ -51,7 +49,7 @@ public class quicksort
 	fin = System.currentTimeMillis();
 	tiempo = (double) ((fin - inicio));
     System.out.print(tiempo +" milisegundos");
-    printa(array);
+    //printa(array);
 	System.out.println();
         
 	shuffle(array, top);
@@ -62,7 +60,7 @@ public class quicksort
 	fin = System.currentTimeMillis();
 	tiempo = (double) ((fin - inicio));
     System.out.print(tiempo +" milisegundos");
-    printa(array);
+    //printa(array);
 	System.out.println();
 
 	shuffle(array, top);
@@ -73,7 +71,7 @@ public class quicksort
 	fin = System.currentTimeMillis();
 	tiempo = (double) ((fin - inicio));
     System.out.print(tiempo +" milisegundos");
-    printa(array);
+    //printa(array);
 	System.out.println();
 
 
@@ -194,50 +192,122 @@ public static void quickSort2k(int[] arr, int low, int high)
 //------------------------------------------------------- Quicksort + insertSort -------------------------------------------------------------------------
 public static void quickSort_Insert_mediana(int[] arr, int low, int high) 
   {
-	int k=16; //k es el numero en el que los arreglos menores serán pasados por insertSort
-	while (low < high) {
-		if (high - low < k) {
-			insertionSort(arr, low, high);
-			break;
-		}
-		else {
-			int pivot = partition2k(arr, low, high);
-			if (pivot - low < pivot - high) {
-				quickSort_InsertSort(arr, low, pivot - 1);
-				low = pivot + 1;
-			}
-			else {
-				quickSort_InsertSort(arr, pivot + 1, high);
-				high = pivot - 1;
-			}
-		}
+	//Ver si el arreglo está vacio
+  int pivot=0;
+  int middle=0;
+  if (arr == null || arr.length == 0){
+	return;
+  }
+  if (low >= high){
+	return;
+  }
+  //Obtener el pivote de la mediana de 2k+1
+  int k=5; //declarar k 1=3, 2=5, 3=7, etc...
+  int m=16;
+  
+  if (high - low < m) {
+	insertionSort(arr, low, high);
+   }
+  
+  if(high-low>=2*k+1){	
+	int[] array2 = new int[2*k+1]; //crear un nuevo arreglo para obtener la mediana
+	Random rand = new Random(); 	
+	for(int c=0;c<=array2.length-1;c++){//lenar el nuevo arreglo con elementos al azar
+		array2[c]=arr[rand.nextInt(high-low)+low]; 
 	}
-	
-	
-	
+	insertionSort(array2, 0, array2.length-1); //ordenamos el arreglo nuevo
+	middle = k; //el elemento del medio de nuestro nuevo arreglo es la mediana en este caso el elemento del medio es k
+  	pivot = array2[middle];
+  }
+  else{
+       Random rand = new Random(); 
+	middle = low + (high - low) / 2; 
+  	pivot = arr[rand.nextInt(high-low)+low]; //en caso que no se pueda aplicar mediana
+  }
+   
+  // Izquierda menor al pivote y derecha mayor al pivote
+  int i = low, j = high;
+  while (i <= j) 
+  {
+	//Todos los valores de la izquierda sean menor al pivote
+	while (arr[i] < pivot) 
+	{
+	  i++;
+	}
+	//Valores de la derecha mayores al pivote
+	while (arr[j] > pivot) 
+	{
+	  j--;
+	}
+	//Ver si los valores necesitan intercambiarse
+	//Si lo nocesitan mover los iteradores
+	if (i <= j) 
+	{
+	  swap (arr, i, j);
+	  i++;
+	  j--;
+	}
+  }
+  //Recursion para los subarreglos
+  if (low < j){
+	quickSort(arr, low, j);
+  }
+  if (high > i){
+	quickSort(arr, i, high);
+  }
   }
    
 // ------------------------------------------------------ Quicksort + insertSort + Mediana de 2k+1--------------------------------------------------------
 public static void quickSort_InsertSort(int[] arr, int low, int high) {
-	int k=16; //k es el numero en el que los arreglos menores serán pasados por insertSort
-	while (low < high) {
-		if (high - low < k) {
-			insertionSort(arr, low, high);
-			break;
-		}
-		else {
-			int pivot = partition(arr, low, high);
-			if (pivot - low < pivot - high) {
-				quickSort_InsertSort(arr, low, pivot - 1);
-				low = pivot + 1;
-			}
-			else {
-				quickSort_InsertSort(arr, pivot + 1, high);
-				high = pivot - 1;
-			}
-		}
-	}
-	
+    int m=16;
+    //Ver si el arreglo está vacio
+    if (arr == null || arr.length == 0){
+      return;
+    }
+    if (low >= high){
+      return;
+    }
+    
+    if (high - low < m) {
+	insertionSort(arr, low, high);
+   }
+    //Obtener el pivote de la mitad del arreglo
+    //int middle = low + (high - low) / 2;
+	//int middle=high; //quicksort normal usando el ultimo elemento como pivote
+    Random rand=new Random();
+     //pivote escojido al azar
+    int pivot = arr[rand.nextInt(high-low)+low];
+ 
+    // Izquierda menor al pivote y derecha mayor al pivote
+    int i = low, j = high;
+    while (i <= j) 
+    {
+      //Todos los valores de la izquierda sean menor al pivote
+      while (arr[i] < pivot) 
+      {
+        i++;
+      }
+      //Valores de la derecha mayores al pivote
+      while (arr[j] > pivot) 
+      {
+        j--;
+      }
+      //Ver si los valores necesitan intercambiarse
+      //Si lo nocesitan mover los iteradores
+      if (i <= j) 
+      {
+        swap (arr, i, j);
+        i++;
+        j--;
+      }
+    }
+    //Recursion para los subarreglos
+    if (low < j){
+      quickSort(arr, low, j);
+    }
+    if (high > i){
+      quickSort(arr, i, high);
+    }
 	
   }
 
@@ -257,7 +327,8 @@ public static void quickSort_InsertSort(int[] arr, int low, int high) {
 			}
 		}
 	}
-	public static int partition(int arr[], int low,int high){
+	
+        /*public static int partition(int arr[], int low,int high){
         Random rand= new Random();
         //int pivot = arr[rand.nextInt(high-low)+low];
         int pivot = arr[high];
@@ -310,7 +381,7 @@ public static void quickSort_InsertSort(int[] arr, int low, int high) {
             }
         }
         return j - 1;
-    }
+    }*/
   public static void swap (int array[], int x, int y)
     {
     int temp = array[x];
