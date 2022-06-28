77fd063ed66120b516dca365e3fccefba0dbdbfd
Notand105 <urzua.fernando.f@gmail.com>
2022-06-28 15:00:34 -0400

    primer paso caluclar operaciones

--- Proyecto d progra/pruebas2.py
+++ Proyecto d progra/pruebas2.py
@@ -1,20 +1,7 @@
 
-cadena="3^9+10/2-15+43*32+6^2"
+cadena="((15+3)+3)+(2+6)^2"
 
-def PartirPor(string, separators):
-        lis = []
-        current = ""
-        for ch in string:
-            if ch in separators:
-                lis.append(current)
-                lis.append(ch)
-                current = ""
-            else:
-                current += ch
-        lis.append(current)
-        return lis
-
-def Resultado(cadena):
+def toArray(cadena):
     array=[]
     for i in cadena:
         array.append(i)
@@ -26,15 +13,50 @@ def Resultado(cadena):
                 if contador>=1:
                     if array[contador-1].isnumeric():
                         array[contador-1:contador+1]= ["".join(array[contador-1:contador+1])]
+                    
             contador+=1
-    print()
-    Operaciones(ToInt(array))
+    return array
+
+def Resultado(array):
     print(array)
+    #array=ToInt(array)
+    while("(" in array):
+        array=cleanPar(array)
+    print(array)
+    array=ToInt(array)
+    Operaciones(array)
+    if(len(array)==1):
+        return array[0]
+    else :return array
+
+def cleanPar(array):
+    cont=0
+    first=0
+    last=0
+    j=0
+    r=0
+    if("(" in array):
+        #totalidad de los parentesis
+        for i in array:
+            if i=="(":
+                cont=+1
+                if(cont==1):first=j
+            if i==")":
+                cont-=1
+                if(cont==0):last=j
+            j+=1
+        array[first]=str(cleanPar(array[first+1:last]))
+        while(r<last-first):
+            array.pop(first+1)
+            r+=1
+    else:
+        return Resultado(array)
+    return array
 
 def ToInt(array):
     for i in array:
-        if (array[array.index(i)].isnumeric()):
-            array[array.index(i)]=int(array[array.index(i)])
+        if (array[array.index(i)].isnumeric() or is_Number(array[array.index(i)])):
+            array[array.index(i)]=float(array[array.index(i)])
     return array
 
 def Operaciones(array):
@@ -98,6 +120,12 @@ def Operaciones(array):
                 
         print(array) 
 
+def is_Number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-
-Resultado(cadena)
+cadena=toArray(cadena)
+print(Resultado(cadena))
